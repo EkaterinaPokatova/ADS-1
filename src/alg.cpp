b0ded8aeef05851758f61b165ca86bab042d3b48
Ekaterina Pokatova <150853636+EkaterinaPokatova@users.noreply.github.com>
2024-03-01 18:52:08 +0300

    Pokatova

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 bool checkPrime(uint64_t value) {
     bool flag = true;
-    if(value < 2){
+    if (value < 2) {
         flag = false;
     }
     for (uint64_t i = 2; i < value; i++) {
@@ -19,9 +19,9 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
     uint64_t result = 1;
-    for(uint64_t i = 1; i <= n; i++){
-        for(uint64_t k = result + 1; k <= INT64_MAX; k++){
-            if(checkPrime(k)){
+    for (uint64_t i = 1; i <= n; i++) {
+        for (uint64_t k = result + 1; k <= INT64_MAX; k++) {
+            if (checkPrime(k)) {
                 result = k;
                 break;
             }
@@ -32,8 +32,8 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
     uint64_t result = 0;
-    for(uint64_t i = value + 1; i <= INT64_MAX; i++){
-        if(checkPrime(i)){
+    for (uint64_t i = value + 1; i <= INT64_MAX; i++) {
+        if (checkPrime(i)) {
             result = i;
             break;
             }
@@ -43,8 +43,8 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t result = 0;
-    for (uint64_t i = 2; i < hbound; i++){
-        if(checkPrime(i)){
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
             result += i;
         }
     }
